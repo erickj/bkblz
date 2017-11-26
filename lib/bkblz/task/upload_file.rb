aed792250a562e9586e58b958ad426e8f030b6b7
Erick Johnson <erick@ejjohnson.org>
2017-11-26 23:53:57 +0100

    adds --file_name flag to upload file task to specifiy the remote file name

--- lib/bkblz/task/upload_file.rb
+++ lib/bkblz/task/upload_file.rb
@@ -4,7 +4,10 @@ module Bkblz
 
       task_param :bucket_name, :required => true
       task_param :file_path, :required => true
+      task_param :file_name # Overrides local file_path if given
 
+      # TODO(erick): Change file_path to a byte string, let the task be agnostic
+      # to where the data comes from. (Probably v0.2).
       def run_internal(session, params)
         f = ::File.new(params[:file_path], "r")
 
@@ -12,7 +15,7 @@ module Bkblz
         upload_auth = session.send(
           Bkblz::V1::GetUploadUrlRequest.new bucket.bucket_id).to_model
 
-        file_name = ::File.basename f.path
+        file_name = params[:file_name] || ::File.basename(f.path)
         file_body = f.read
         mtime_millis = f.mtime.to_i * 1000
 
