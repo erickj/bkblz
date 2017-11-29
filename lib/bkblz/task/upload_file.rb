ec4fe9ee9b7414d97d24d389b453e2062e2274bd
Erick Johnson <erick@ejjohnson.org>
2017-11-29 21:47:14 +0100

    allow uploads from STDIN, see Usage:   bkblz file upload <bucket_name> <local_file_path>
    Options:
      [--file-name=FILE_NAME]  # remote file name, if not given the basenameof the local file is used
    
    upload a local file to B2

--- lib/bkblz/task/upload_file.rb
+++ lib/bkblz/task/upload_file.rb
@@ -3,25 +3,41 @@ module Bkblz
     class UploadFile < BaseTask
 
       task_param :bucket_name, :required => true
-      task_param :file_path, :required => true
+      task_param :file_body # Either file_body or file_path is required
+      task_param :file_path
       task_param :file_name # Overrides local file_path if given
 
-      # TODO(erick): Change file_path to a byte string, let the task be agnostic
-      # to where the data comes from. (Probably v0.2).
       def run_internal(session, params)
-        f = ::File.new(params[:file_path], "r")
+        file_body = if params[:file_path]
+                      f = ::File.new(params[:file_path], "r")
+                      f.read
+                    elsif params[:file_body]
+                      params[:file_body]
+                    else
+                      raise 'missing either :file_body or :file_path param'
+                    end
+
+        file_name = if params[:file_name]
+                      params[:file_name]
+                    elsif params[:file_path]
+                      ::File.basename(params[:file_path])
+                    else
+                      raise 'missing either :file_name or :file_path param'
+                    end
+
+        file_mtime = if params[:file_path]
+                       ::File.mtime(params[:file_path])
+                     else
+                       Time.now
+                     end.to_i * 1000
 
         bucket = find_bucket_by_name session, params[:bucket_name]
         upload_auth = session.send(
           Bkblz::V1::GetUploadUrlRequest.new bucket.bucket_id).to_model
 
-        file_name = params[:file_name] || ::File.basename(f.path)
-        file_body = f.read
-        mtime_millis = f.mtime.to_i * 1000
-
         upload_file_info = session.send(
-          Bkblz::V1::UploadFileRequest.new upload_auth, file_body, file_name,
-                                           nil, mtime_millis).to_model
+          Bkblz::V1::UploadFileRequest.new(
+            upload_auth, file_body, file_name, nil, file_mtime)).to_model
       end
     end
   end
